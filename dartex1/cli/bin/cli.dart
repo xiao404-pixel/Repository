b358a80c27aa2baeb88c581e3e20b3696e5c9baf
xiao404-pixel <riverc8763@gmail.com>
2026-04-09 12:29:28 +0800

    好難寫!!

--- dartex1/cli/bin/cli.dart
+++ dartex1/cli/bin/cli.dart
@@ -1,5 +1,27 @@
-//import 'package:cli/cli.dart' as cli;
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  print('Hello dart');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchwikipedia(inputArgs);
+
+    print('Search command recognized!');
+  } else {
+    printUsage();
+  }
+}
+
+void searchwipedia(List<String>? arguments) {
+  print('searchwikipedia received argments: $arguments');
+}
+
+void printUsage() {
+  // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
 }
